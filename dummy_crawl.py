46d4d1610303d46bf539517ba600e9039d355fbb
Dennis Dubrefjord <Dubrefjord@protonmail.com>
2021-03-24 16:50:40 +0100

    added a comment

--- dummy_crawl.py
+++ dummy_crawl.py
@@ -4,7 +4,8 @@ import os
 
 if sys.argv[2] == '-M':
   wfd = int(os.environ['crawler_write_fd'])
-  crawler_output = os.fdopen(wfd, "w",buffering=1)
+  crawler_output = os.fdopen(wfd, "w",buffering=1) #Makes sure the file is flushed after every newline char
+  #The buffering rules could probably be improved further when we know the format of the data.
 
 os.write(wfd,str.encode("first node\n"))
 time.sleep(5)
